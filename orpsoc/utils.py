c593439c736fe301b453812f948758ff9f7408c8
carlos <carlos@marte.inesc-id.pt>
2014-02-21 16:35:00 +0000

    remove launch, fix bug stderr

--- orpsoc/utils.py
+++ orpsoc/utils.py
@@ -1,14 +1,17 @@
 import subprocess
 
 class Launcher:
-    def __init__(self, cmd, args=[], shell=False, cwd=None, stderr=None, errormsg=None, env=None):
+    def __init__(self, cmd, args=[], shell=False, cwd=None, stderr_path=None, errormsg=None, env=None):
         self.cmd      = cmd
         self.args     = args
         self.shell    = shell
         self.cwd      = cwd
-        self.stderr   = stderr
         self.errormsg = errormsg
         self.env      = env
+        self.stderr_path = stderr_path
+        self.stderr = None
+        if stderr_path != None:
+            self.stderr = open(self.stderr_path, 'w')	
 
     def run(self):
         try:
@@ -21,10 +24,10 @@ class Launcher:
         except OSError:
             raise RuntimeError("Error: Command " + self.cmd + " not found. Make sure it is in $PATH")
         except subprocess.CalledProcessError:
-            if self.stderr is None:
-                self.stderr = "stderr"
+            if self.stderr_path is None:
+                self.stderr_path = "stderr"
             if self.errormsg is None:
-                self.errormsg = '"' + str(self) + '" exited with an error code. See ' + self.stderr + ' for details'
+                self.errormsg = '"' + str(self) + '" exited with an error code. See ' + self.stderr_path + ' for details'
             raise RuntimeError(self.errormsg)
 
     def __str__(self):
@@ -45,21 +48,3 @@ def convert_V2H( read_file, write_file):
             fC.close
             fV.close
         
-def launch(cmd, args=[], shell=False, cwd=None, stderr=None):
-    stderr_file = None
-    if stderr != None
-        stderr_file = open(stderr, 'w')
-
-    try:
-        subprocess.check_call([cmd] + args,
-                              cwd = cwd,
-                              shell = shell,
-                              stderr = stderr_file),
-    except OSError:
-        print("Error: Command " + cmd + " not found. Make sure it is in $PATH")
-        exit(1)
-    except subprocess.CalledProcessError:
-        if stderr is None:
-            stderr = "stderr"
-        print("Error: " + cmd + ' '.join(args) + " returned errors. See " + stderr + " for details")
-        exit(1)
